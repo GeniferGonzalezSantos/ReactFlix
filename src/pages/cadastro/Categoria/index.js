cdd9707be8571312b3abc8aabfa7d69b432b9183
GeniferGonzalezSantos <genh_@hotmail.com>
2020-07-28 19:14:07 -0300

    adicionou form a categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import PageDefault from '../../../components/PageDefault';
+import InputForm from '../../../components/Form/components/Input';
+import ButtonForm   from '../../../components/Form/components/ButtonForm';
+import Select from '../../../components/Form/components/Select';
+import TextArea from '../../../components/Form/components/TextArea';
+import '../../../components/Form/styles.css';
+
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
-      <Link to='/'>
-        Ir para a Home
-      </Link>
+      <h1>Nova Categoria</h1>
+      <form>
+        <fieldset>
+          <label for="nome">
+            <InputForm type='text' placeholder='Nome' />
+          </label>
+          <label for="área de texto">
+            <TextArea type='text' placeholder='Vídeo' />
+          </label>
+          <label for="seleção de cores">
+            <Select />
+          </label>
+          <label for="código de seguraça">
+            <InputForm type='text' placeholder='Código de segurança' />
+          </label>
+          <div className='btn-position'>
+          <ButtonForm className='buttonColor'>Salvar</ButtonForm>
+          <ButtonForm>Limpar</ButtonForm>
+        </div>
+        </fieldset>
+      </form>
+
+      <Link to="/">
+        Ir para home
+    </Link>
     </PageDefault>
   )
 };
