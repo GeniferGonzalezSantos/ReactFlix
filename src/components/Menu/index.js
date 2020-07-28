bac6e52affb3e32b871d287d469b3d2a1666a988
GeniferGonzalezSantos <genh_@hotmail.com>
2020-07-28 15:17:13 -0300

    adicinou arquivo slider, pasta Home e Videos e instalou a lib slick

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import Button from '../Button'
 import './Menu.css';
@@ -7,11 +8,11 @@ import './Menu.css';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="geniFlix logo" />
       
-      </a>
-      <Button className="ButtonLink" href="/">
+      </Link>
+      <Button  as={Link} className="ButtonLink" to="/cadastro/video">
       Novo vídeo
       </Button>
     </nav>
